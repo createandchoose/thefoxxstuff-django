f47784911ac53b5f881306b9338989e9771db095
Aytal Popov <foxx_remix@mail.ru>
2021-06-09 01:20:58 +0900

    asd
    asd

--- thefoxxstuff/thefoxxstuff/urls.py
+++ thefoxxstuff/thefoxxstuff/urls.py
@@ -1,17 +1,17 @@
-"""thefoxxstuff URL Configuration
+"""Конфигурация URL-адреса foxxstuff
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+Список " URL-шаблоны` направляет URL-адреса в представления. Для получения дополнительной информации, пожалуйста, смотрите:
+https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Примеры:
+Представления функций
+1. Добавьте импорт: из представлений импорта my_app
+2. Добавьте URL-адрес в urlpatterns: path(", views.home, name='home')
+Представления на основе классов
+1. Добавьте импорт: из other_app.views import Home
+2. Добавьте URL-адрес в url-шаблоны: path(", Home.as_view(), name='home')
+Включая другой URLconf
+1. Импортируйте функцию include (): из django.urls импортируйте include, путь
+2. Добавьте URL-адрес в url-шаблоны: path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path
