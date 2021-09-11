0eb00c91580d7443e1ab21af15c9807353686af6
Aytal Popov <foxx_remix@mail.ru>
2021-09-11 23:37:31 +0900

    dfg
    ddf

--- thefoxxstuff/main/views.py
+++ thefoxxstuff/main/views.py
@@ -7,7 +7,6 @@ from django.views import generic
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
-    
 
 class DetailView(generic.DetailView):
     model = Post
